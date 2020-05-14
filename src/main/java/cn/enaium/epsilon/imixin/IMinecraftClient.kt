5b5b3c24b5a655a8e01502c3e008a6adbdb1d5ca
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-14 16:23:19 +0800

    Update IMinecraftClient.kt

--- src/main/java/cn/enaium/epsilon/imixin/IMinecraftClient.kt
+++ src/main/java/cn/enaium/epsilon/imixin/IMinecraftClient.kt
@@ -1,14 +1,12 @@
-package cn.enaium.epsilon.imixin;
+package cn.enaium.epsilon.imixin
 
-import net.minecraft.client.util.Window;
+import net.minecraft.client.util.Window
 
 /**
  * Project: Epsilon
  * -----------------------------------------------------------
  * Copyright © 2020 | Enaium | All rights reserved.
  */
-public interface IMinecraftClient {
-
-    Window window();
-
+interface IMinecraftClient {
+    fun window(): Window
 }
