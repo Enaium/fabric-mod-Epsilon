e08e1f1129b2ab96427fd63de9d51d41dbadd2bf
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-14 12:22:25 +0800

    Create ColorUtils.kt

---
+++ src/main/java/cn/enaium/epsilon/utils/ColorUtils.kt
@@ -0,0 +1,14 @@
+package cn.enaium.epsilon.utils
+
+import java.awt.Color
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+object ColorUtils {
+    val SELECT = Color(33, 170, 47).rgb
+    val BG = Color(23, 23, 23).rgb
+    val DISABLE = Color(75, 75, 75).rgb
+}
