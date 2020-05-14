43dd7cec5f51e2e1d0c6d38883d6b801b2ba6b01
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-14 12:24:34 +0800

    EpsilonMod.java to EpsilonMod.kt

--- src/main/java/cn/enaium/epsilon/EpsilonMod.kt
+++ src/main/java/cn/enaium/epsilon/EpsilonMod.kt
@@ -1,14 +1,9 @@
-package cn.enaium.epsilon;
+package cn.enaium.epsilon
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ModInitializer
 
-public class EpsilonMod implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
-        System.out.println("Hello Epsilon world!");
+class EpsilonMod : ModInitializer {
+    override fun onInitialize() {
+        println("Hello Epsilon world!")
     }
 }
