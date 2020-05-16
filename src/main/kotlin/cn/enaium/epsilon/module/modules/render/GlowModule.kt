31707abfcbd29b7802c3b22b57dc9abb3d69bdaf
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-16 19:55:23 +0800

    Update GlowModule.kt

--- src/main/kotlin/cn/enaium/epsilon/module/modules/render/GlowModule.kt
+++ src/main/kotlin/cn/enaium/epsilon/module/modules/render/GlowModule.kt
@@ -6,6 +6,19 @@ import cn.enaium.epsilon.event.events.EventUpdate
 import cn.enaium.epsilon.module.Category
 import cn.enaium.epsilon.module.Module
 import cn.enaium.epsilon.module.ModuleAT
+import cn.enaium.epsilon.setting.SettingAT
+import cn.enaium.epsilon.setting.settings.EnableSetting
+import net.minecraft.entity.Entity
+import net.minecraft.entity.ItemEntity
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.mob.*
+import net.minecraft.entity.passive.AnimalEntity
+import net.minecraft.entity.passive.IronGolemEntity
+import net.minecraft.entity.passive.VillagerEntity
+import net.minecraft.entity.passive.WolfEntity
+import net.minecraft.entity.player.PlayerEntity
+import java.util.stream.Stream
+import java.util.stream.StreamSupport
 
 /**
  * Project: Epsilon
@@ -14,6 +27,7 @@ import cn.enaium.epsilon.module.ModuleAT
  */
 @ModuleAT
 class GlowModule : Module("Glow", 0, Category.RENDER) {
+
     @EventAT
     fun onUpdate(eventUpdate: EventUpdate) {
         for (e in Epsilon.MC.world!!.entities) {
