9f1cbfda742785b878de0a3f9451324a720245c3
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-16 19:55:37 +0800

    Add NoFall

---
+++ src/main/kotlin/cn/enaium/epsilon/module/modules/movement/NoFallModule.kt
@@ -0,0 +1,26 @@
+package cn.enaium.epsilon.module.modules.movement
+
+import cn.enaium.epsilon.Epsilon.MC
+import cn.enaium.epsilon.event.EventAT
+import cn.enaium.epsilon.event.events.EventUpdate
+import cn.enaium.epsilon.module.Category
+import cn.enaium.epsilon.module.Module
+import cn.enaium.epsilon.module.ModuleAT
+import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
+
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+@ModuleAT
+class NoFallModule : Module("NoFall", 0, Category.MOVEMENT) {
+    @EventAT
+    fun on(eventUpdate: EventUpdate) {
+        if (MC.player!!.fallDistance <= 2) return
+
+        MC.player!!.networkHandler.sendPacket(PlayerMoveC2SPacket(true))
+    }
+}
