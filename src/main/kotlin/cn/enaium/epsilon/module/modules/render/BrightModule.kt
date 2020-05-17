3585105a00234e230940bd008451c2c7bc509ead
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-17 11:37:07 +0800

    Bright add NightVision

--- src/main/kotlin/cn/enaium/epsilon/module/modules/render/BrightModule.kt
+++ src/main/kotlin/cn/enaium/epsilon/module/modules/render/BrightModule.kt
@@ -1,9 +1,17 @@
 package cn.enaium.epsilon.module.modules.render
 
 import cn.enaium.epsilon.Epsilon
+import cn.enaium.epsilon.Epsilon.MC
+import cn.enaium.epsilon.event.EventAT
+import cn.enaium.epsilon.event.events.EventRender2D
 import cn.enaium.epsilon.module.Category
 import cn.enaium.epsilon.module.Module
 import cn.enaium.epsilon.module.ModuleAT
+import cn.enaium.epsilon.setting.SettingAT
+import cn.enaium.epsilon.setting.settings.ModeSetting
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+
 
 /**
  * Project: Epsilon
@@ -12,13 +20,21 @@ import cn.enaium.epsilon.module.ModuleAT
  */
 @ModuleAT
 class BrightModule : Module("Bright", 0, Category.RENDER) {
-    override fun onEnable() {
-        Epsilon.MC.options.gamma = 300.0
-        super.onEnable()
+
+    @SettingAT
+    private val mode = ModeSetting(this, "Mode", "Gamma", arrayListOf("Gamma", "NightVision"))
+
+    @EventAT
+    fun on(eventRender2D: EventRender2D) {
+        when (mode.current) {
+            "Gamma" -> MC.options.gamma = 300.0
+            "NightVision" -> MC.player!!.addStatusEffect(StatusEffectInstance(StatusEffects.NIGHT_VISION, 16360, 0, false, false))
+        }
     }
 
     override fun onDisable() {
-        Epsilon.MC.options.gamma = 1.0
         super.onDisable()
+        MC.options.gamma = 1.0
+        MC.player!!.removeStatusEffectInternal(StatusEffects.NIGHT_VISION)
     }
 }
