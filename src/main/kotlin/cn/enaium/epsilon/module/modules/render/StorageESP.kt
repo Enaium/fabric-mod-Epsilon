fbf640d4460d592b521eddec1e27e1f399b3dedd
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-18 19:48:15 +0800

    Complete StorageESP

--- src/main/kotlin/cn/enaium/epsilon/module/modules/render/StorageESP.kt
+++ src/main/kotlin/cn/enaium/epsilon/module/modules/render/StorageESP.kt
@@ -8,9 +8,13 @@ import cn.enaium.epsilon.module.Module
 import cn.enaium.epsilon.module.ModuleAT
 import cn.enaium.epsilon.setting.SettingAT
 import cn.enaium.epsilon.setting.settings.EnableSetting
+import cn.enaium.epsilon.utils.BlockUtils
+import cn.enaium.epsilon.utils.Render3DUtils
 import net.minecraft.block.entity.*
-import java.util.stream.Stream
-import java.util.stream.StreamSupport
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import org.lwjgl.opengl.GL11
+import java.awt.Color
 
 /**
  * Project: Epsilon
@@ -33,43 +37,65 @@ class StorageESP : Module("StorageESP", 0, Category.RENDER) {
     private val shulkerBoxChest = EnableSetting(this, "ShulkerBoxChest", true)
 
     @SettingAT
-    private val barrel = EnableSetting(this, "Barrel", true)
-
-    @SettingAT
     private val hopper = EnableSetting(this, "Hopper", true)
 
     @SettingAT
     private val furnace = EnableSetting(this, "Furnace", true)
 
-    @EventAT
-    fun onRender(render3DEvent: Render3DEvent) {
+    @SettingAT
+    private val dispenser = EnableSetting(this, "Dispenser", true)
 
+    @SettingAT
+    private val dropper = EnableSetting(this, "Dropper", true)
+
+    @SettingAT
+    private val barrel = EnableSetting(this, "Barrel", true)
+
+    @SettingAT
+    private val blastFurnace = EnableSetting(this, "BlastFurnace", true)
+
+    @SettingAT
+    private val smoker = EnableSetting(this, "Smoker", true)
+
+    override fun onEnable() {
+        super.onEnable()
+        GL11.glNewList(1, GL11.GL_COMPILE)
+        val bb = Box(BlockPos.ORIGIN)
+        Render3DUtils.drawOutlined(bb)
+        GL11.glEndList()
     }
 
-    private fun getTargets(): Stream<BlockEntity> {
-        var s = StreamSupport.stream(Epsilon.MC.world!!.blockEntities.spliterator(), true)
-        if (!chest.enable) {
-            s = s.filter { it !is ChestBlockEntity }
+    override fun onDisable() {
+        super.onDisable()
+        GL11.glDeleteLists(1, 1)
+    }
+
+    @EventAT
+    fun onRender(render3DEvent: Render3DEvent) {
+        Render3DUtils.applyRenderOffset()
+        for (t in getTargets()) {
+            Render3DUtils.drawOutlinedBox(BlockUtils.getBoundingBox(t.pos), Color.DARK_GRAY)
         }
-        if (!trappedChest.enable) {
-            s = s.filter { it !is TrappedChestBlockEntity }
+    }
+
+    private fun getTargets(): ArrayList<BlockEntity> {
+        val blocks: ArrayList<BlockEntity> = ArrayList()
+        for (be in Epsilon.MC.world!!.blockEntities) {
+            when (be) {
+                is ChestBlockEntity -> if (chest.enable) blocks.add(be)
+                is TrappedChestBlockEntity -> if (trappedChest.enable) blocks.add(be)
+                is EnderChestBlockEntity -> if (enderChest.enable) blocks.add(be)
+                is ShulkerBoxBlockEntity -> if (shulkerBoxChest.enable) blocks.add(be)
+                is HopperBlockEntity -> if (hopper.enable) blocks.add(be)
+                is DispenserBlockEntity -> if (dispenser.enable) blocks.add(be)
+                is DropperBlockEntity -> if (dropper.enable) blocks.add(be)
+                is FurnaceBlockEntity -> if (furnace.enable) blocks.add(be)
+                is BarrelBlockEntity -> if (barrel.enable) blocks.add(be)
+                is BlastFurnaceBlockEntity -> if (blastFurnace.enable) blocks.add(be)
+                is SmokerBlockEntity -> if (smoker.enable) blocks.add(be)
+            }
         }
-        if (!enderChest.enable) {
-            s = s.filter { it !is EnderChestBlockEntity }
-        }
-        if (!shulkerBoxChest.enable) {
-            s = s.filter { it !is ShulkerBoxBlockEntity }
-        }
-        if (!barrel.enable) {
-            s = s.filter { it !is BarrelBlockEntity }
-        }
-        if (!hopper.enable) {
-            s = s.filter { it !is HopperBlockEntity }
-        }
-        if (!furnace.enable) {
-            s = s.filter { it !is FurnaceBlockEntity }
-        }
-        return s
+        return blocks
     }
 
 }
