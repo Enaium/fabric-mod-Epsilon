4b03e0fd800129997eb5992f3d7c145291b37f63
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-19 10:34:49 +0800

    Add AutoRespawn

---
+++ src/main/kotlin/cn/enaium/epsilon/func/functions/other/AutoRespawn.kt
@@ -0,0 +1,25 @@
+package cn.enaium.epsilon.func.functions.other
+
+import cn.enaium.epsilon.Epsilon.MC
+import cn.enaium.epsilon.event.EventAT
+import cn.enaium.epsilon.event.events.UpdateEvent
+import cn.enaium.epsilon.func.Category
+import cn.enaium.epsilon.func.Func
+import cn.enaium.epsilon.func.FuncAT
+import net.minecraft.client.gui.screen.DeathScreen
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+@FuncAT
+class AutoRespawn : Func("AutoRespawn", 0, Category.OTHER) {
+    @EventAT
+    fun on(updateEvent: UpdateEvent) {
+        if (MC.currentScreen is DeathScreen) {
+            MC.player!!.requestRespawn()
+            MC.openScreen(null)
+        }
+    }
+}
