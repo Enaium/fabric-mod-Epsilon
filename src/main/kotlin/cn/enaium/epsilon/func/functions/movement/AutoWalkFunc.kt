332d0e16af796ceab585755e0037110703cace16
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-19 14:37:05 +0800

    Add AutoWalk

---
+++ src/main/kotlin/cn/enaium/epsilon/func/functions/movement/AutoWalkFunc.kt
@@ -0,0 +1,28 @@
+package cn.enaium.epsilon.func.functions.movement
+
+import cn.enaium.epsilon.Epsilon.MC
+import cn.enaium.epsilon.event.EventAT
+import cn.enaium.epsilon.event.events.UpdateEvent
+import cn.enaium.epsilon.func.Category
+import cn.enaium.epsilon.func.Func
+import cn.enaium.epsilon.func.FuncAT
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+@FuncAT
+class AutoWalkFunc : Func("AutoWalk", 0, Category.MOVEMENT) {
+
+    @EventAT
+    fun on(updateEvent: UpdateEvent) {
+        MC.options.keyForward.isPressed = true
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+        MC.options.keyForward.isPressed = false
+    }
+
+}
