294edb3728055a10879506a8a0331ad28bf0f6fb
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-21 16:23:57 +0800

    Add Xray

---
+++ src/main/java/cn/enaium/epsilon/mixin/BlockModelRendererMixin.java
@@ -0,0 +1,52 @@
+package cn.enaium.epsilon.mixin;
+
+import cn.enaium.epsilon.event.events.ShouldDrawSideEvent;
+import cn.enaium.epsilon.event.events.TessellateBlockEvent;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.block.BlockModelRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+@Mixin(BlockModelRenderer.class)
+public class BlockModelRendererMixin {
+    @Inject(at = {@At("HEAD")}, method = {"renderSmooth"}, cancellable = true)
+    private void onRenderSmoothOrFlat(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        TessellateBlockEvent tessellateBlockEvent = new TessellateBlockEvent(state);
+        tessellateBlockEvent.call();
+
+        if (tessellateBlockEvent.getCancelled()) {
+            callbackInfoReturnable.cancel();
+            return;
+        }
+
+        if (!cull)
+            return;
+
+        ShouldDrawSideEvent shouldDrawSideEvent = new ShouldDrawSideEvent(state);
+        shouldDrawSideEvent.call();
+        if (!Boolean.TRUE.equals(shouldDrawSideEvent.getRendered()))
+            return;
+
+        renderSmooth(world, model, state, pos, buffer, vertexConsumer, false, random, seed, overlay);
+    }
+
+    @Shadow
+    public boolean renderSmooth(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay) {
+        return false;
+    }
+}
---
+++ src/main/kotlin/cn/enaium/epsilon/func/functions/render/XrayFunc.kt
@@ -0,0 +1,77 @@
+package cn.enaium.epsilon.func.functions.render
+
+import cn.enaium.epsilon.Epsilon.MC
+import cn.enaium.epsilon.event.EventAT
+import cn.enaium.epsilon.event.events.RenderBlockEntityEvent
+import cn.enaium.epsilon.event.events.ShouldDrawSideEvent
+import cn.enaium.epsilon.event.events.TessellateBlockEvent
+import cn.enaium.epsilon.func.Category
+import cn.enaium.epsilon.func.Func
+import cn.enaium.epsilon.func.FuncAT
+import cn.enaium.epsilon.utils.BlockUtils
+import net.minecraft.block.Block
+import org.lwjgl.glfw.GLFW
+import java.util.*
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+@FuncAT
+class XrayFunc : Func("Xray", GLFW.GLFW_KEY_X, Category.RENDER) {
+
+    val blocks = arrayListOf("Ores", "",
+            "minecraft:anvil", "minecraft:beacon", "minecraft:bone_block",
+            "minecraft:bookshelf", "minecraft:brewing_stand",
+            "minecraft:chain_command_block", "minecraft:chest", "minecraft:clay",
+            "minecraft:coal_block", "minecraft:coal_ore", "minecraft:command_block",
+            "minecraft:crafting_table", "minecraft:diamond_block",
+            "minecraft:diamond_ore", "minecraft:dispenser", "minecraft:dropper",
+            "minecraft:emerald_block", "minecraft:emerald_ore",
+            "minecraft:enchanting_table", "minecraft:end_portal",
+            "minecraft:end_portal_frame", "minecraft:ender_chest",
+            "minecraft:furnace", "minecraft:glowstone", "minecraft:gold_block",
+            "minecraft:gold_ore", "minecraft:hopper", "minecraft:iron_block",
+            "minecraft:iron_ore", "minecraft:ladder", "minecraft:lapis_block",
+            "minecraft:lapis_ore", "minecraft:lava", "minecraft:mossy_cobblestone",
+            "minecraft:nether_portal", "minecraft:nether_quartz_ore",
+            "minecraft:redstone_block", "minecraft:redstone_ore",
+            "minecraft:repeating_command_block", "minecraft:spawner",
+            "minecraft:tnt", "minecraft:torch", "minecraft:trapped_chest",
+            "minecraft:water", "minecraft:white_bed")
+
+    override fun onEnable() {
+        super.onEnable()
+        MC.worldRenderer.reload()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+        MC.worldRenderer.reload()
+    }
+
+    @EventAT
+    fun shouldDrawSideEvent(shouldDrawSideEvent: ShouldDrawSideEvent) {
+        shouldDrawSideEvent.rendered = isXray(shouldDrawSideEvent.blockState.block)
+    }
+
+    @EventAT
+    fun tessellateBlockEvent(tessellateBlockEvent: TessellateBlockEvent) {
+        if (isXray(tessellateBlockEvent.blockState.block)) {
+            tessellateBlockEvent.cancel()
+        }
+    }
+
+    @EventAT
+    fun renderBlockEntityEvent(renderBlockEntityEvent: RenderBlockEntityEvent) {
+        if (isXray(BlockUtils.getBlock(renderBlockEntityEvent.blockEntity.pos))) {
+            renderBlockEntityEvent.cancel()
+        }
+    }
+
+    private fun isXray(block: Block): Boolean {
+        val name = BlockUtils.getName(block)
+        return Collections.binarySearch(blocks, name) >= 0
+    }
+}
