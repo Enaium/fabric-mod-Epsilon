659c347a327a62b21dee0666eed0b244683f2229
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-26 22:43:17 +0800

    Create LockAngleFunc.kt

---
+++ src/main/kotlin/cn/enaium/epsilon/func/functions/render/LockAngleFunc.kt
@@ -0,0 +1,30 @@
+package cn.enaium.epsilon.func.functions.render
+
+import cn.enaium.epsilon.event.Event
+import cn.enaium.epsilon.event.EventAT
+import cn.enaium.epsilon.event.events.MotionEvent
+import cn.enaium.epsilon.func.Category
+import cn.enaium.epsilon.func.Func
+import cn.enaium.epsilon.setting.SettingAT
+import cn.enaium.epsilon.setting.settings.FloatSetting
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+class LockAngleFunc : Func("LockAngle", 0, Category.RENDER) {
+
+    @SettingAT
+    val yaw = FloatSetting(this, "Yaw", 0F, -180F, 180F)
+
+    @SettingAT
+    val pitch = FloatSetting(this, "Pitch", 0F, -90F, 90F)
+
+    @EventAT
+    fun on(motionEvent: MotionEvent) {
+        motionEvent.yaw = yaw.current
+        motionEvent.pitch = pitch.current
+    }
+
+}
