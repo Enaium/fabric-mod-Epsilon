7e0047dea8952541f42f4961675c69cd509c60c6
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-26 22:43:03 +0800

    Enhance MotionEvent

--- src/main/kotlin/cn/enaium/epsilon/event/events/MotionEvent.kt
+++ src/main/kotlin/cn/enaium/epsilon/event/events/MotionEvent.kt
@@ -1,10 +1,11 @@
 package cn.enaium.epsilon.event.events
 
 import cn.enaium.epsilon.event.Event
+import net.minecraft.util.math.Vec3d
 
 /**
  * Project: Epsilon
  * -----------------------------------------------------------
  * Copyright © 2020 | Enaium | All rights reserved.
  */
-class MotionEvent(type: Type) : Event(type)
+class MotionEvent(type: Type, var yaw: Float, var pitch: Float, var ground: Boolean, var vec3d: Vec3d) : Event(type)
