bf40a430e4be4da6645cbd32b14e9ac341ff3a20
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-30 08:16:29 +0800

    Update pre2

--- src/main/kotlin/cn/enaium/epsilon/Epsilon.kt
+++ src/main/kotlin/cn/enaium/epsilon/Epsilon.kt
@@ -18,7 +18,7 @@ import java.io.File
  */
 object Epsilon {
     var NAME = "Epsilon"
-    var VERSION = "pre1"
+    var VERSION = "pre2"
     var GAME = "20w22a"
     var AUTHOR = "Enaium"
     var DIR = MinecraftClient.getInstance().runDirectory.toString() + "/" + NAME + "/"
