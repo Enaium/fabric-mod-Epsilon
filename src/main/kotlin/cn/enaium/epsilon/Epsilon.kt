cf7e1e56f921e1c3e73dde107d4c9d0e275ef4e0
Enaium <32991121+Enaium@users.noreply.github.com>
2020-06-13 20:58:56 +0800

    Update 1.16-pre5

--- src/main/kotlin/cn/enaium/epsilon/Epsilon.kt
+++ src/main/kotlin/cn/enaium/epsilon/Epsilon.kt
@@ -18,8 +18,8 @@ import java.io.File
  */
 object Epsilon {
     var NAME = "Epsilon"
-    var VERSION = "pre5"
-    var GAME = "1.16-pre4"
+    var VERSION = "pre7"
+    var GAME = "1.16-pre5"
     var AUTHOR = "Enaium"
     var DIR = MinecraftClient.getInstance().runDirectory.toString() + "/" + NAME + "/"
     var MC: MinecraftClient = MinecraftClient.getInstance()
