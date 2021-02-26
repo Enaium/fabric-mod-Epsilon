7362a3cae0eb595a75bf5a43a5f26d15cd426e8f
Enaium <32991121+Enaium@users.noreply.github.com>
2021-02-26 14:06:21 +0800

    NoCooldown

---
+++ src/main/kotlin/cn/enaium/epsilon/client/func/functions/combat/NoCooldown.kt
@@ -0,0 +1,12 @@
+package cn.enaium.epsilon.client.func.functions.combat
+
+import cn.enaium.cf4m.annotation.module.Module
+import cn.enaium.cf4m.module.Category
+
+/**
+ * Project: Epsilon
+ * -----------------------------------------------------------
+ * Copyright © 2020-2021 | Enaium | All rights reserved.
+ */
+@Module("NoCooldown", category = Category.COMBAT)
+class NoCooldown
